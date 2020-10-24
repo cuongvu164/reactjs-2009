089afa2379c12315df2b96eda2e79ea34f73d3df
cuongcuoi <cuongmk288@gmail.com>
2020-10-24 22:54:59 +0700

    add lai

--- bai-tap-buoi-10/main.js
+++ bai-tap-buoi-10/main.js
@@ -100,13 +100,13 @@ const studentObject = (firstArray, secondArray) => {
 
 //Bài 8:
 const students = [
-    { id: 1, name: 'Nguyễn Thị Tèo', score: 9.2 },
-    { id: 2, name: 'Phạm Văn Bưởi', score: 2.3 },
-    { id: 3, name: 'Hoàng Văn Nam', score: 3.7 },
-    { id: 4, name: 'Vũ Ngọc Duy', score: 6.9 },
-    { id: 5, name: 'Nguyễn Minh Nhật', score: 5.2 },
-    { id: 6, name: 'Phí Duy Quân', score: 9.6 },
-    { id: 7, name: 'Trần Minh Minh', score: 6.1 }
+  { id: 1, name: 'Nguyễn Thị Tèo', score: 9.2 },
+  { id: 2, name: 'Phạm Văn Bưởi', score: 2.3 },
+  { id: 3, name: 'Hoàng Văn Nam', score: 3.7 },
+  { id: 4, name: 'Vũ Ngọc Duy', score: 6.9 },
+  { id: 5, name: 'Nguyễn Minh Nhật', score: 5.2 },
+  { id: 6, name: 'Phí Duy Quân', score: 9.6 },
+  { id: 7, name: 'Trần Minh Minh', score: 6.1 }
 ]
 const findMinMax = array => {
   let theBest = []
